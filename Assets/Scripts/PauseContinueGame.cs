d59ad9c97f8dff6cc216596f1ee5761f92ac715f
Troy <troy.lamerton@gmail.com>
2017-04-11 23:31:08 +0200

    Get ESC to pause working

--- Assets/Scripts/PauseContinueGame.cs
+++ Assets/Scripts/PauseContinueGame.cs
@@ -1,32 +1,30 @@
-﻿public class Pause : MonoBehaviour 
-{
-	[SerializeField] private GameObject pausePanel;
-	void Start()
-	{
+﻿using UnityEngine;
+
+public class PauseContinueGame : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pausePanel;
+
+	void Start() {
 		pausePanel.SetActive(false);
+		Debug.Log ("esceeas");
 	}
-	void Update()
-	{
-		if(Input.GetKeyDown (KeyCode.Escape)) 
-		{
-			if (!pausePanel.activeInHierarchy) 
-			{
+	void Update() {
+		if(Input.GetKeyDown (KeyCode.Escape)) {
+			Debug.Log ("esc");
+			if (!pausePanel.activeInHierarchy) {
 				PauseGame();
-			}
-			if (pausePanel.activeInHierarchy) 
-			{
+			} else {
 				ContinueGame();   
 			}
 		} 
 	}
-	private void PauseGame()
-	{
+	void PauseGame() {
 		Time.timeScale = 0;
 		pausePanel.SetActive(true);
 		//Disable scripts that still work while timescale is set to 0
 	} 
-	private void ContinueGame()
-	{
+	void ContinueGame()	{
 		Time.timeScale = 1;
 		pausePanel.SetActive(false);
 		//enable the scripts again
